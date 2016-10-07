e712782459aec4d579cf35c39c836e168056f38d
Fabien <you@example.com>
2016-10-07 16:55:57 +0200

    Ménage

--- CFW8/bin/Release/CFW8 Test.js
+++
@@ -1,6 +0,0 @@
-//*** CHECK THIS ProgID ***
-var X = new ActiveXObject("ASCOM.CFW8.FilterWheel");
-WScript.Echo("This is " + X.Name + ")");
-// You may want to uncomment this...
-// X.Connected = true;
-X.SetupDialog();
